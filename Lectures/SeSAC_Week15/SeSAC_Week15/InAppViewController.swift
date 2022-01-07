e06e0dadfbc9a5f2f06896332e2a1f50ee20c458
JD-man <ekgkzmtm@gmail.com>
2022-01-07 16:45:19 +0900

    In App Purchase

---
+++ Lectures/SeSAC_Week15/SeSAC_Week15/InAppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InAppViewController.swift
+//  SeSAC_Week15
+//
+//  Created by JD_MacMini on 2022/01/07.
+//
+
+import UIKit
+
+class InAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
