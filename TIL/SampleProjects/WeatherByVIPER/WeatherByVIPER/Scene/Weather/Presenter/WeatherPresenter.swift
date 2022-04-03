744f304d4fd6cdea65c7afc69d5ac53850b3699a
JD-man <ekgkzmtm@gmail.com>
2022-04-03 17:54:47 +0900

    Add: VIPER example WeatherPresenter Binding.

--- TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/Scene/Weather/Presenter/WeatherPresenter.swift
+++ TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/Scene/Weather/Presenter/WeatherPresenter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxRelay
+
+final class WeatherPresenter: WeatherPresenterType {
+    
+    private var disposeBag = DisposeBag()
+    
+    struct Input {
+        let viewDidLoad = PublishRelay<Void>()
+    }
+    
+    struct Output {
+        let weatherResponse = PublishRelay<WeatherResponseModel>()
+    }
+    
+    var router: RouterType
+    var interactor: WeatherInteractorType
+    
+    var input = Input()
+    var output = Output()
+    
+    init(router: RouterType, interactor: WeatherInteractorType) {
+        self.router = router
+        self.interactor = interactor
+        binding()
+    }
+    
+    func binding() {
+        input.viewDidLoad
+            .withUnretained(self)
+            .flatMapLatest { (presenter, signal) in
+                presenter.interactor.fetchWeatherData()
+            }.bind(to: output.weatherResponse)
+            .disposed(by: disposeBag)
+    }
+}
