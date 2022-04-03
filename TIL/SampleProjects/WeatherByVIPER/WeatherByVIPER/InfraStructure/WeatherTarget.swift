9304ec863c2822bd6cb252617417d367c10a30b4
JD-man <ekgkzmtm@gmail.com>
2022-04-03 17:53:33 +0900

    Add: VIPER example Moya WeahterTarget

--- TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/InfraStructure/WeatherTarget.swift
+++ TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/InfraStructure/WeatherTarget.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Moya
+
+typealias Parameters = [String: Any]
+
+enum WeatherTarget {
+    case getWeatherInfo(parameters: Parameters)
+}
+
+
+extension WeatherTarget: TargetType {
+    var baseURL: URL {
+        return URL(string:URLComponents.rootURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getWeatherInfo:
+            return "/data/2.5/weather"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getWeatherInfo:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getWeatherInfo(let parameters):
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .getWeatherInfo:
+            return nil
+        }
+    }
+    
+    var validationType: ValidationType {
+        return .customCodes([200])
+    }
+}
