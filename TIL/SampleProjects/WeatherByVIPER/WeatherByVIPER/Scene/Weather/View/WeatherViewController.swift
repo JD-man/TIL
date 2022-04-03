bbcd7b457c049572464fd76638ca0c89db74abf4
JD-man <ekgkzmtm@gmail.com>
2022-04-03 17:54:08 +0900

    Add: VIPER example WeatherVC Binding.

--- TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/Scene/Weather/View/WeatherViewController.swift
+++ TIL/SampleProjects/WeatherByVIPER/WeatherByVIPER/Scene/Weather/View/WeatherViewController.swift
@@ -6,20 +6,32 @@
 //
 
 import UIKit
+import RxSwift
 import SnapKit
 
-// 신림역 - 37.48433605595456, 126.92985099999653
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, WeatherViewType {
     
     private let temperatureLabel = WeatherBaseLabel()
     private let humidityLabel = WeatherBaseLabel()
     private let speedLabel = WeatherBaseLabel()
     private let iconImageView = WeatherBaseImageView()
-
+    
+    var presenter: WeatherPresenter
+    private var disposeBag = DisposeBag()
+    
+    init(presenter: WeatherPresenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewConfig()
+        binding()
     }
     
     private func viewConfig() {
@@ -48,6 +60,32 @@ class WeatherViewController: UIViewController {
             make.top.equalTo(speedLabel.snp.bottom).offset(baseOffset)
             make.width.height.equalTo(100)
         }
+    }
+    
+    private func binding() {        
+        Observable.just(())
+            .bind(to: presenter.input.viewDidLoad)
+            .disposed(by: disposeBag)
+        
+        presenter.output.weatherResponse
+            .map { "\($0.main.temp)" }
+            .bind(to: temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        presenter.output.weatherResponse
+            .map { "\($0.main.humidity)" }
+            .bind(to: humidityLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        presenter.output.weatherResponse
+            .map { "\($0.wind.speed)" }
+            .bind(to: speedLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        presenter.output.weatherResponse
+            .map { $0.weather[0].icon }
+            .bind(to: iconImageView.rx.iconImage)
+            .disposed(by: disposeBag)
     }
 }
 
